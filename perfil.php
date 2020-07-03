31fd894669de75231daf211fb7b2590ad11b399c
Roberto Costa <robertocosta66@gmail.com>
2020-07-03 17:59:35 -0300

    Modal bug removed

--- perfil.php
+++ perfil.php
@@ -150,18 +150,22 @@ require 'partials/menu.php';
                 </div>
             </div>
             <div class="box-body row m-20">
+
                 <?php if(count($user->photos) > 0 ):?>
                     <?php foreach($user->photos as $key=>$item):?>
-                        <div class="user-photo-item">
-                            <a href="#modal-<?=$key;?>" rel="modal:open">
-                                <img src="<?=$base;?>/media/uploads/<?=$item->body;?>" />
-                            </a>
-                            <div id="modal-<?=$key;?>" style="display:none">
-                                <img src="<?=$base;?>/media/uploads/<?=$item->body;?>" />
+                        <?php if($key < 4):?>
+                            <div class="user-photo-item">
+                                <a href="#modal-<?=$key;?>" data-modal-open>
+                                    <img src="<?=$base;?>/media/uploads/<?=$item->body;?>" />
+                                </a>
+                                <div id="modal-<?=$key;?>" style="display:none">
+                                    <img src="<?=$base;?>/media/uploads/<?=$item->body;?>" />
+                                </div>
                             </div>
-                        </div>
+                        <?php endif; ?>
                     <?php endforeach;?>
-                <?php endif;?>                
+                <?php endif;?>  
+
             </div>
                 
         </div>
@@ -180,4 +184,9 @@ require 'partials/menu.php';
     </div>    
 </div>
 </section>
+<script>
+    window.onload = function() {
+        var modal = new VanillaModal.default();
+    };
+</script>
 <?php require 'partials/footer.php';?>
